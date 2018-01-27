3a102b6ef84209e1dbb978afc751d73a2f7be8b5
Alex Bazhanau <bazhanau@outlook.com>
2018-01-27 13:34:45 +0100

    ERC223 Transfer event fix

--- contracts/token/ERC223.sol
+++ contracts/token/ERC223.sol
@@ -4,5 +4,5 @@ contract ERC223 {
     function transfer(address _to, uint _value, bytes _data) public returns (bool);
     function transferFrom(address _from, address _to, uint256 _value, bytes _data) public returns (bool);
     
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
