e3d7a42cb180ce8712dd16976bfd59f2bab98742
Alex Bazhanau <bazhanau@outlook.com>
2018-02-11 23:52:29 +0300

    truffle.js updated

--- truffle.js
+++ truffle.js
@@ -9,7 +9,16 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: 42,
-      gasPrice: 1000000000
+      gasPrice: 1000000000,
+      from: "0x001D51cDC8f4B378e136642DdB95Dfc4fF6a4B72"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4712388,
+      gasPrice: 30000000000,
+      from: "0xb4Ea4cBD466c8991b0B45290F529AF4957c25a53"
     },
     live: {
       host: "localhost",
@@ -17,6 +26,13 @@ module.exports = {
       network_id: 1,
       gasPrice: 10000000000,
       from: "0x0"
+    }
+  },
+
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 }
